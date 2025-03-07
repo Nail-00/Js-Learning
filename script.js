5bc6475bb6c70f5157b27de3b3f6f1b5bafd14d7
Nail Məmmədov <ziya77nesiroon77@gmail.com>
2025-03-08 01:43:12 +0400

    2

--- script.js
+++ script.js
@@ -1,11 +1,65 @@
-let isim = "Nail";
+let a =18;
+if ( a%2 ==0 ) {
+    console.log("Cut sayidir");
+}else {
+    console.log("Tek sayidir");
+}
 
-isim = "Falon";
-console.log (typeof isim + " " + isim);
 
-const dogumyili = 2004;
+let b =25;
+if (b < 18){
+    console.log("resitsiniz");
+}else(b >= 18);{
+    console.log("resit degilsiniz");
+}
 
-console.log (typeof dogumyili + " " + dogumyili);
+let c = 5;
+switch (c) {
+    case 1:
+        console.log("Pazartesi");
+        break;
+    case 2:
+        console.log("Salı");
+        break;
+    case 3:
+        console.log("Çarşamba");
+        break;
+    case 4:
+        console.log("Perşembe");
+        break;
+    case 5:
+        console.log("Cuma");
+        break;
+    case 6:
+        console.log("Cumartesi");
+        break;
+    case 7:
+        console.log("Pazar");
+        break;
+    default:
+        console.log("Hatalı giriş");
+        break;
+}
 
-var cinsiyeti = true;
-console.log (typeof cinsiyeti + " " + cinsiyeti);
+let Notlar = "C";
+
+switch (Notlar) {
+    case "A":
+        console.log("Mükemmel");
+        break;
+    case "B":
+        console.log("iyi");
+        break;
+    case "C":
+        console.log("Orta");
+        break;
+    case "D":
+        console.log("Geçer");
+        break;
+    case "F":
+        console.log("Başarısız");
+        break;
+    default:
+        console.log("Hatalı giriş");
+        break;
+}
