be90159cbaecb0591b2171798bb5748615914b04
Nail Məmmədov <ziya77nesiroon77@gmail.com>
2025-03-08 00:50:59 +0400

    1

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+let isim = "Nail";
 
+isim = "Falon";
+console.log (typeof isim + " " + isim);
+
+const dogumyili = 2004;
+
+console.log (typeof dogumyili + " " + dogumyili);
+
+var cinsiyeti = true;
+console.log (typeof cinsiyeti + " " + cinsiyeti);
