621c17130d7541514ea85abd4f3cbd15aed82051
Nail Məmmədov <ziya77nesiroon77@gmail.com>
2025-03-08 17:19:39 +0400

    3

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-let a = 10;
-for (let a = 0; a < 20; a++) {
-    
-    
+let toplam = 0;
+
+for (let i = 10; i <= 20; i++) {
+    toplam += i; // toplam = toplam + i
 }
+
+console.log("10 ile 20 arasındaki sayıların toplamı:", toplam);
 
 
 let b = 5;
@@ -12,3 +14,12 @@ while (b > 1) {
     console.log(b);
 }
 
+
+let sayi;
+
+do {
+    sayi = prompt("Lütfen 10 ile 50 arasında bir sayı girin:");
+    sayi = Number(sayi); // Kullanıcının girdiğini sayıya çeviriyoruz
+} while (sayi < 10 || sayi > 50); // Eğer sayı 10 ile 50 arasında değilse, tekrar sorar
+
+console.log("Tebrikler! Geçerli bir sayı girdiniz:", sayi);
