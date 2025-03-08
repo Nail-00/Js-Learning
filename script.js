e4bcadccbc5535466045c47bbb81d3a062e6a161
Nail Məmmədov <ziya77nesiroon77@gmail.com>
2025-03-08 18:26:24 +0400

    3.2.1

--- script.js
+++ script.js
@@ -8,15 +8,16 @@ console.log(toplam);
 let sayi = Number(prompt("Bir sayı girin:"));
 let asal = true;
 
-if (sayi < 2) {
-    asal = false; // 2'den küçük sayılar asal olamaz
-} else {
-    for (let i = 2; i < sayi; i++) {
+if (sayi > 2) {
+    
+     for (let i = 2; i < sayi; i++) {
         if (sayi % i === 0) {
             asal = false; // Eğer tam bölünürse asal değildir
             break;
         }
     }
+} else {
+   asal = false; // 2'den küçük sayılar asal olamaz
 }
 
 if (asal) {
